a62a37ef75047b96331ca84433606406dce29c8d
Lux459 <108336474+Lux459@users.noreply.github.com>
2026-04-13 11:50:30 +0200

    Hehe je commit farm

---
+++ notam_groupeA/SiteNotam/views.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, session, jsonify
+app = Flask(__name__)
+
+app.template_folder = "template"
+app.static_folder = "static"
+app.config.from_object('SiteNotam.config')
+
+# page accueil
+@app.route("/")
+def index(): 
+    return render_template("index.html")
+
+# page se connecter
+@app.route("/notam")
+def notam():
+    return render_template("notam.html")
+
+# page sgbd
+@app.route("/vols")
+def vols():
+    return render_template("vols.html")
+
+# gestion des fichiers
+@app.route("/compte")
+def compte():
+    return render_template("compte.html")
---
+++ notam_groupeA/run.py
@@ -0,0 +1,9 @@
+from SiteNotam.config import DEBUG, WEB_SERVER
+from SiteNotam.views import app
+
+if __name__ == '__main__':
+    app.run(
+        host = WEB_SERVER['host'], 
+        port=  WEB_SERVER['port'],
+        debug=DEBUG
+    )
