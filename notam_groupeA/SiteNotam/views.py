7c27127325f70bfaae975c7d62e742f02ae87b9b
ABC <arkamali150@gmail.com>
2026-05-12 12:00:54 +0200

    BONJOUR LES AMIS VIVE LA DGAC

--- notam_groupeA/SiteNotam/views.py
+++ notam_groupeA/SiteNotam/views.py
@@ -1,3 +1,10 @@
+import secrets
+import string
+import hashlib
+from flask import flash
+
+
+
 from flask import Flask, render_template, request, redirect, session, jsonify
 app = Flask(__name__)
 
@@ -29,3 +36,35 @@ def compte():
 @app.route("/signin")
 def signin():
     return render_template("signin.html")
+
+
+
+
+@app.route("/addmdp")
+def addmdp():
+    rform = request.form
+    msg={
+        "ok": "Mot de passe valide",
+        "echec" : "Mot de passe invalide"
+    }
+    
+    #genere mdp aléatoire de 5 caractères
+    caracteres = string.ascii_letters + string.digits
+    mdp = ''.join(secrets.choice(caracteres) for _ in range(5))
+    print("Mot de passe généré:", mdp)
+    
+    #Chiffrement SHA-256
+    mdp_hash = hashlib.sha256(mdp.encode()).hexdigest()
+    print("Mot de passe chiffré:", mdp_hash)
+   
+    
+    #création user
+    idUser = bdd.add_userData(rform, mdp_hash, msg)
+    print(idUser)
+    flash(mdp)
+    return redirect("/modifMdp")
+
+
+@app.route("/modifMdp")
+def modifMdp():
+    return render_template("modifMdp.html")
